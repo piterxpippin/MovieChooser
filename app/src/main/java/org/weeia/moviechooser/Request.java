db1001ba98d3a44e9a5154f5a67cff85925de9c0
Adrian <adrian.gawinek@wp.pl>
2016-05-21 08:15:30 +0200

    added request activity

--- app/src/main/java/org/weeia/moviechooser/Request.java
+++ app/src/main/java/org/weeia/moviechooser/Request.java
@@ -1,19 +1,39 @@
 package org.weeia.moviechooser;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import moviefinder.dao.elasticsearch.ElasticSearchMovieSource;
 import moviefinder.domain.Movie;
 import moviefinder.domain.MovieType;
 
 public class Request extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "org.weeia.moviechooser.MOVIES";
+
+    ElasticSearchMovieSource elasticSearchMovieSource;
+    List<Movie> movieList = new ArrayList<Movie>();
+    MovieType movieType;
+    RadioButton exactRate;
+    RatingBar ratingbar;
+    EditText title;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,26 +42,156 @@ public class Request extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        exactRate = (RadioButton)findViewById(R.id.exactRate);
+        ratingbar = (RatingBar)findViewById(R.id.ratingBar);
+        title = (EditText)findViewById(R.id.editText_title);
+        ratingbar.setRating(4.0f);
 
-        ElasticSearchMovieSource elasticSearchMovieSource= new ElasticSearchMovieSource("192.168.0.108",9300);
 
+        //elasticSearchMovieSource = new ElasticSearchMovieSource("localhost",9300);
 
         //List<Movie> moviesWithGivenGenreSortedByNumberOfVotes = elasticSearchMovieSource.getMoviesWithGivenGenreSortedByNumberOfVotes(MovieType.DRAMA, 0, 0, 50);
         //elasticSearchMovieSource.getMoviesWithGivenGenreAndMinimumNumberOfRatings(MovieType.DRAMA,50,0,50);
-        List<Movie> moviesWithGivenGenreAndAverageRatingStratingFrom = elasticSearchMovieSource.getMoviesWithGivenGenreAndAverageRatingStratingFrom(MovieType.DRAMA, 0.0f, 0, 50, 50);
-        System.out.print(moviesWithGivenGenreAndAverageRatingStratingFrom);
+        //List<Movie> moviesWithGivenGenreAndAverageRatingStratingFrom = elasticSearchMovieSource.getMoviesWithGivenGenreAndAverageRatingStratingFrom(MovieType.DRAMA, 0.0f, 0, 50, 50);
+        //System.out.print(moviesWithGivenGenreAndAverageRatingStratingFrom);
 
 
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_request);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if(title.getText().toString().contentEquals("")){
+                    if(exactRate.isChecked()){
+                        //movieList = elasticSearchMovieSource.getMoviesWithGivenGenreAndAverageRatingStratingFrom(movieType, ratingbar.getRating(),0,50,20);
+                        Snackbar.make(view, "search by exaxt", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }else {
+                        //movieList = elasticSearchMovieSource.getMoviesWithGivenGenreAndMinimumNumberOfRatings(movieType, Math.round(ratingbar.getRating()), 0, 50);
+                        Snackbar.make(view, "search my min", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                }else{
+                    //movieList.add(elasticSearchMovieSource.getMovieByTitle(title.getText().toString()));
+                    Snackbar.make(view, "search by title", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+
+
+
+
+                Intent intent = new Intent(getBaseContext(), SearchResult.class);
+                Bundle extra = new Bundle();
+                extra.putSerializable("listOfMovies", (Serializable) movieList);
+                intent.putExtra(EXTRA_MESSAGE, extra);
+                //startActivity(intent);
             }
         });
     }
+
+    public void onRadioButtonClicked(View view){
+
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()){
+            case R.id.radio_comedy:
+                if(checked){
+                    movieType = MovieType.COMEDY;
+                }
+                break;
+            case R.id.radio_drama:
+                if(checked){
+                    movieType = MovieType.DRAMA;
+                }
+                break;
+            case R.id.radio_thriller:
+                if(checked){
+                    movieType = MovieType.THRILLER;
+                }
+                break;
+            case R.id.radio_crime:
+                if(checked){
+                    movieType = MovieType.CRIME;
+                }
+                break;
+            case R.id.radio_war:
+                if(checked){
+                    movieType = MovieType.WAR;
+                }
+                break;
+            case R.id.radio_action:
+                if(checked){
+                    movieType = MovieType.ACTION;
+                }
+                break;
+            case R.id.radio_adventure:
+                if(checked){
+                    movieType = MovieType.ADVENTURE;
+                }
+                break;
+            case R.id.radio_romance:
+                if(checked){
+                    movieType = MovieType.ROMANCE;
+                }
+                break;
+            case R.id.radio_sci_fi:
+                if(checked){
+                    movieType = MovieType.SCI_FI;
+                }
+                break;
+            case R.id.radio_film_noir:
+                if(checked){
+                    movieType = MovieType.FILM_NOIR;
+                }
+                break;
+            case R.id.radio_imax:
+                if(checked){
+                    movieType = MovieType.IMAX;
+                }
+                break;
+            case R.id.radio_mystery:
+                if(checked){
+                    movieType = MovieType.MYSTERY;
+                }
+                break;
+            case R.id.radio_western:
+                if(checked){
+                    movieType = MovieType.WESTERN;
+                }
+                break;
+            case R.id.radio_fantasy:
+                if(checked){
+                    movieType = MovieType.FANTASY;
+                }
+                break;
+            case R.id.radio_animation:
+                if(checked){
+                    movieType = MovieType.ANIMATION;
+                }
+                break;
+            case R.id.radio_children:
+                if(checked){
+                    movieType = MovieType.CHILDREN;
+                }
+                break;
+            case R.id.radio_musical:
+                if(checked){
+                    movieType = MovieType.MUSICAL;
+                }
+                break;
+            case R.id.radio_horror:
+                if(checked){
+                    movieType = MovieType.HORROR;
+                }
+                break;
+            case R.id.radio_documentary:
+                if(checked){
+                    movieType = MovieType.DOCUMENTARY;
+                }
+                break;
+        }
+    }
+
 
 }
